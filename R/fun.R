f810109c90772f51a9084948632ea4e60b72cc96
Eirik Berger <eirik.berger@nhh.no>
2023-01-19 20:34:36 +0100

    function fixes (vectorized)

--- R/fun.R
+++ R/fun.R
@@ -19,43 +19,42 @@ FromKomnrCounty <- function(komnr_string, type='countynr'){
 #' FromKomnrCountyname('1205')
 #'
 #' @export
-FromKomnrCountyname <- function(komnr_string, type='countynr'){
+FromKomnrCountyname <- Vectorize(function(komnr_string, type='countynr'){
   tmp <- as.character(komnr_string)
   num_county <- as.integer(stringr::str_match(tmp, "^([0-9]{1,2})[0-9]{2}")[,2])
   string_county <- '(missing)'
 
-  if (length(num_county)>1){num_county=999}
-  if (is.na(num_county)){num_county=999}
+  try(if (num_county==1){string_county <- 'Østfold'})
+  try(if (num_county==2){string_county <- 'Akershus'})
+  try(if (num_county==3){string_county <- 'Oslo'})
 
-  if (num_county==1){string_county = 'Østfold'}
-  if (num_county==2){string_county = 'Akershus'}
-  if (num_county==3){string_county = 'Oslo'}
+  try(if (num_county==4){string_county <- 'Hermark'})
+  try(if (num_county==5){string_county <- 'Oppland'})
+  try(if (num_county==6){string_county <- 'Buskerud'})
 
-  if (num_county==4){string_county = 'Hermark'}
-  if (num_county==5){string_county = 'Oppland'}
-  if (num_county==6){string_county = 'Buskerud'}
+  try(if (num_county==7){string_county <- 'Vestfold'})
+  try(if (num_county==8){string_county <- 'Telemark'})
+  try(if (num_county==9){string_county <- 'Aust-Agder'})
 
-  if (num_county==7){string_county = 'Vestfold'}
-  if (num_county==8){string_county = 'Telemark'}
-  if (num_county==9){string_county = 'Aust-Agder'}
+  try(if (num_county==10){string_county <- 'Vest-Agder'})
+  try(if (num_county==11){string_county <- 'Rogaland'})
+  try(if (num_county==12){string_county <- 'Hordaland'})
 
-  if (num_county==10){string_county = 'Vest-Agder'}
-  if (num_county==11){string_county = 'Rogaland'}
-  if (num_county==12){string_county = 'Hordaland'}
+  try(if (num_county==13){string_county <- 'Bergen'})
+  try(if (num_county==14){string_county <- 'Sogn og Fjordane'})
+  try(if (num_county==15){string_county <- 'Møre og Romsdal'})
 
-  if (num_county==13){string_county = 'Bergen'}
-  if (num_county==14){string_county = 'Sogn og Fjordane'}
-  if (num_county==15){string_county = 'Møre og Romsdal'}
+  try(if (num_county==16){string_county <- 'Sør-Trøndelag'})
+  try(if (num_county==17){string_county <- 'Nord-Trøndelag'})
+  try(if (num_county==18){string_county <- 'Nordland'})
 
-  if (num_county==16){string_county = 'Sør-Trøndelag'}
-  if (num_county==17){string_county = 'Nord-Trøndelag'}
-  if (num_county==18){string_county = 'Nordland'}
+  try(if (num_county==19){string_county <- 'Troms'})
+  try(if (num_county==20){string_county <- 'Finnmark'})
 
-  if (num_county==19){string_county = 'Troms'}
-  if (num_county==20){string_county = 'Finnmark'}
+  print(num_county)
 
   return(string_county)
-}
+})
 
 #' @param komnr_string municipality number.
 #' @return An integer
