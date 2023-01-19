60d8e29a0dded6b045887d73c7a1d6ab727b302e
Eirik Berger <eirik.berger@nhh.no>
2023-01-19 16:18:09 +0100

    now works with missings

--- R/fun.R
+++ R/fun.R
@@ -20,38 +20,40 @@ FromKomnrCounty <- function(komnr_string, type='countynr'){
 #'
 #' @export
 FromKomnrCountyname <- function(komnr_string, type='countynr'){
-  library(stringr)
   tmp <- as.character(komnr_string)
-  num_county <- as.integer(str_match(tmp, "^([0-9]{1,2})[0-9]{2}")[,2])
+  num_county <- as.integer(stringr::str_match(tmp, "^([0-9]{1,2})[0-9]{2}")[,2])
   string_county <- '(missing)'
-  
+
+  if (length(num_county)>1){num_county=999}
+  if (is.na(num_county)){num_county=999}
+
   if (num_county==1){string_county = 'Østfold'}
   if (num_county==2){string_county = 'Akershus'}
   if (num_county==3){string_county = 'Oslo'}
-  
+
   if (num_county==4){string_county = 'Hermark'}
   if (num_county==5){string_county = 'Oppland'}
   if (num_county==6){string_county = 'Buskerud'}
-  
+
   if (num_county==7){string_county = 'Vestfold'}
   if (num_county==8){string_county = 'Telemark'}
   if (num_county==9){string_county = 'Aust-Agder'}
-  
+
   if (num_county==10){string_county = 'Vest-Agder'}
   if (num_county==11){string_county = 'Rogaland'}
   if (num_county==12){string_county = 'Hordaland'}
-  
+
   if (num_county==13){string_county = 'Bergen'}
   if (num_county==14){string_county = 'Sogn og Fjordane'}
   if (num_county==15){string_county = 'Møre og Romsdal'}
-  
+
   if (num_county==16){string_county = 'Sør-Trøndelag'}
   if (num_county==17){string_county = 'Nord-Trøndelag'}
   if (num_county==18){string_county = 'Nordland'}
-  
+
   if (num_county==19){string_county = 'Troms'}
   if (num_county==20){string_county = 'Finnmark'}
-  
+
   return(string_county)
 }
 
